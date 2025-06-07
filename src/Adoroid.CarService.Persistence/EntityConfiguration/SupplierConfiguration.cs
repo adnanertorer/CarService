cb520771c3406a7e7fca3e250cf853e5f650f309
adnanertorer <adnanertorer@hotmail.com>
2025-06-07 23:02:12 +0300

    SupplierConfiguration

--- src/Adoroid.CarService.Persistence/EntityConfiguration/SupplierConfiguration.cs
+++ src/Adoroid.CarService.Persistence/EntityConfiguration/SupplierConfiguration.cs
@@ -4,17 +4,16 @@ using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace Adoroid.CarService.Persistence.EntityConfiguration;
 
-public class EmployeeConfiguration : IEntityTypeConfiguration<Employee>
+public class SupplierConfiguration : IEntityTypeConfiguration<Supplier>
 {
-    public void Configure(EntityTypeBuilder<Employee> builder)
+    public void Configure(EntityTypeBuilder<Supplier> builder)
     {
         builder.HasKey(b => b.Id);
         builder.Property(b => b.Name).IsRequired().HasMaxLength(50);
-        builder.Property(b => b.Surname).IsRequired().HasMaxLength(50);
+        builder.Property(b => b.ContactName).IsRequired().HasMaxLength(150);
         builder.Property(b => b.PhoneNumber).IsRequired().HasMaxLength(20);
         builder.Property(b => b.Email).HasMaxLength(60);
         builder.Property(b => b.Address).HasMaxLength(250);
-        builder.Property(b => b.IsActive).IsRequired();
         builder.Property(b => b.CompanyId).IsRequired();
 
         builder.Property(i => i.CreatedDate).IsRequired();
