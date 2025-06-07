58552d79351be972cd32a43b7f189ef259e926cd
adnanertorer <adnanertorer@hotmail.com>
2025-06-07 13:44:12 +0300

    CarServiceDbContext, CarServiceDbContextFactory

---
+++ src/Adoroid.CarService.Persistence/CarServiceDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Adoroid.CarService.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace Adoroid.CarService.Persistence;
+
+public class CarServiceDbContext(DbContextOptions<CarServiceDbContext> options): DbContext(options)
+{
+    public DbSet<Company> Companies { get; set; }
+    public DbSet<User> Users { get; set; }
+    public DbSet<Employee> Employees { get; set; }
+    public DbSet<MainService> MainServices { get; set; }
+    public DbSet<Customer> Customers { get; set; }
+    public DbSet<Vehicle> Vehicles { get; set; }
+    public DbSet<Supplier> Suppliers { get; set; }
+    public DbSet<SubService> SubServices { get; set; }
+    public DbSet<AccountingTransaction> AccountingTransactions { get; set; }
+
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+   {
+      modelBuilder.ApplyConfigurationsFromAssembly(assembly: Assembly.GetExecutingAssembly());
+    }
+}
---
+++ src/Adoroid.CarService.Persistence/CarServiceDbContextFactory.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace Adoroid.CarService.Persistence;
+
+public class CarServiceDbContextFactory : IDesignTimeDbContextFactory<CarServiceDbContext>
+{
+    public CarServiceDbContext CreateDbContext(string[] args = null)
+    {
+        var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Development";
+        var basePath = Path.Combine(Directory.GetCurrentDirectory(), "../Adoroid.CarService.Api");
+
+        var configuration = new ConfigurationBuilder()
+              .SetBasePath(basePath)
+              .AddJsonFile("appsettings.json")
+              .AddJsonFile($"appsettings.{environment}.json", optional: true)
+              .Build();
+
+        var optionsBuilder = new DbContextOptionsBuilder<CarServiceDbContext>();
+        optionsBuilder.UseNpgsql(configuration.GetConnectionString("DefaultConnectionString"));
+
+        return new CarServiceDbContext(optionsBuilder.Options);
+    }
+}
