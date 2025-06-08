721189c43e4726a73d1de678526330168b808190
adnanertorer <adnanertorer@hotmail.com>
2025-06-08 14:37:24 +0300

    CarServicePersistenceServiceCollection

---
+++ src/Adoroid.CarService.Persistence/CarServicePersistenceServiceCollection.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Adoroid.CarService.Persistence;
+
+public static class CarServicePersistenceServiceCollection
+{
+    public static IServiceCollection AddCarServicePersistence(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<CarServiceDbContext>(options =>
+        {
+            options.UseNpgsql(configuration.GetConnectionString("DefaultConnectionString"),
+                m => m.MigrationsAssembly("Adoroid.CarService.Api"));
+        });
+
+        return services;
+    }
+}
