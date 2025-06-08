0dd106023405c90880d88a9986a230d5ad73b1dd
adnanertorer <adnanertorer@hotmail.com>
2025-06-08 14:44:03 +0300

    ICurrentUser

---
+++ src/Adoroid.CarService.Application/CurrentUser.cs
@@ -0,0 +1,29 @@
+﻿using Adoroid.CarService.Shared;
+using Microsoft.AspNetCore.Http;
+using System.Security.Claims;
+
+namespace Adoroid.CarService.Application;
+
+public class CurrentUser(IHttpContextAccessor httpContextAccessor) : ICurrentUser
+{
+    public string? Id => httpContextAccessor.HttpContext?.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+    public string? UserName => httpContextAccessor.HttpContext?.User.FindFirst(ClaimTypes.Name)?.Value;
+    public string? Email => httpContextAccessor.HttpContext?.User.FindFirst(ClaimTypes.Email)?.Value;
+    public string? CompanyId => httpContextAccessor.HttpContext?.User.FindFirst("CompanyId")?.Value;
+    public List<string>? Roles => httpContextAccessor.HttpContext?.User.FindAll(ClaimTypes.Role).Select(r => r.Value).ToList();
+
+    public string? Token
+    {
+        get
+        {
+            var authorizationHeader = httpContextAccessor.HttpContext?.Request.Headers["Authorization"].ToString();
+            if (!string.IsNullOrEmpty(authorizationHeader) && authorizationHeader.StartsWith("Bearer "))
+            {
+                return authorizationHeader.Substring("Bearer ".Length).Trim();
+            }
+            return null;
+        }
+    }
+
+    
+}
---
+++ src/Adoroid.CarService.Shared/ICurrentUser.cs
@@ -0,0 +1,11 @@
+﻿namespace Adoroid.CarService.Shared;
+
+public interface ICurrentUser
+{
+    string? Id { get; }
+    string? CompanyId { get; }
+    string? UserName { get; }
+    string? Email { get; }
+    string? Token { get; }
+    List<string>? Roles { get; }
+}
