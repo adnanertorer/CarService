e4ad744ed1f441ade49e5b95fac6bab8c4f7436f
adnanertorer <adnanertorer@hotmail.com>
2025-06-08 14:48:42 +0300

    CarServiceApplicationServiceCollection

---
+++ src/Adoroid.CarService.Application/CarServiceApplicationServiceCollection.cs
@@ -0,0 +1,34 @@
+﻿using Adoroid.Core.Application.Rules;
+using FluentValidation;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MinimalMediatR.Behaviors;
+using MinimalMediatR.Core;
+using MinimalMediatR.Extensions;
+using System.Reflection;
+
+namespace Adoroid.CarService.Application;
+
+public static class CarServiceApplicationServiceCollection
+{
+    public static IServiceCollection AddCarServiceApplication(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddSubClassesOfType(Assembly.GetExecutingAssembly(), type: typeof(BaseBusinessRule));
+        services.AddMinimalMediatR(Assembly.GetExecutingAssembly());
+        services.AddTransient(typeof(IPipelineBehavior<,>), typeof(LoggingBehavior<,>));
+        services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+        return services;
+    }
+
+    private static IServiceCollection AddSubClassesOfType(this IServiceCollection services, Assembly assembly, Type type,
+    Func<IServiceCollection, Type, IServiceCollection>? addWithLifeCycle = null)
+    {
+        var types = assembly.GetTypes().Where(t => t.IsSubclassOf(type) && type != t).ToList();
+        foreach (var item in types)
+            if (addWithLifeCycle == null)
+                services.AddScoped(item);
+            else
+                addWithLifeCycle(services, type);
+        return services;
+    }
+}
