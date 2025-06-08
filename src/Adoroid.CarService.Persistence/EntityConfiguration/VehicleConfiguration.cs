f887d7ac02d9693167b654f0edc7bde92bfa0aed
adnanertorer <adnanertorer@hotmail.com>
2025-06-08 14:12:35 +0300

    VehicleConfiguration

---
+++ src/Adoroid.CarService.Persistence/EntityConfiguration/VehicleConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using Adoroid.CarService.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Adoroid.CarService.Persistence.EntityConfiguration;
+
+public class VehicleConfiguration : IEntityTypeConfiguration<Vehicle>
+{
+    public void Configure(EntityTypeBuilder<Vehicle> builder)
+    {
+        builder.HasKey(b => b.Id);
+        builder.Property(b => b.CustomerId).IsRequired();
+        builder.Property(b => b.Brand).IsRequired().HasMaxLength(50);
+        builder.Property(b => b.Model).IsRequired().HasMaxLength(50);
+        builder.Property(b => b.Year).IsRequired();
+        builder.Property(b => b.Plate).IsRequired().HasMaxLength(20);
+        builder.Property(b => b.Engine).HasMaxLength(20);
+        builder.Property(b => b.FuelTypeId).IsRequired();
+        builder.Property(b => b.SerilNumber).HasMaxLength(30);
+
+        builder.Property(i => i.CreatedDate).IsRequired();
+        builder.Property(c => c.CreatedBy).IsRequired().HasMaxLength(64);
+
+        builder.Property(c => c.UpdatedBy).HasMaxLength(64);
+        builder.Property(c => c.DeletedBy).HasMaxLength(64);
+
+        builder.HasQueryFilter(c => c.IsDeleted == false || c.DeletedDate == null);
+    }
+}
