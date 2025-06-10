9893a9cf0e1a5e1794d54dd862d122c28be411f4
adnanertorer <adnanertorer@hotmail.com>
2025-06-10 14:37:30 +0300

    DeleteSupplierCommand

---
+++ src/Adoroid.CarService.Application/Features/Suppliers/Commands/Delete/Validations/DeleteSupplierCommandVadlitor.cs
@@ -0,0 +1,14 @@
+﻿using Adoroid.CarService.Application.Common.ValidationMessages;
+using FluentValidation;
+
+namespace Adoroid.CarService.Application.Features.Suppliers.Commands.Delete.Validations;
+
+public class DeleteSupplierCommandVadlitor : AbstractValidator<DeleteSupplierCommand>
+{
+    public DeleteSupplierCommandVadlitor()
+    {
+        RuleFor(x => x.Id)
+         .NotNull()
+         .WithMessage(string.Format(ValidationMessages.Required, "Id"));
+    }
+}
