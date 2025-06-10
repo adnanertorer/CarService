cc03cdaae27bd3be53efe2a714c1b013dc9c98ce
adnanertorer <adnanertorer@hotmail.com>
2025-06-10 14:50:50 +0300

    SupplierGetByIdRequest

---
+++ src/Adoroid.CarService.Application/Features/Suppliers/Queries/GetById/Validators/SupplierGetByIdRequestValidator.cs
@@ -0,0 +1,14 @@
+﻿using Adoroid.CarService.Application.Common.ValidationMessages;
+using FluentValidation;
+
+namespace Adoroid.CarService.Application.Features.Suppliers.Queries.GetById.Validators;
+
+public class SupplierGetByIdRequestValidator : AbstractValidator<SupplierGetByIdRequest>
+{
+    public SupplierGetByIdRequestValidator()
+    {
+        RuleFor(x => x.Id)
+         .NotNull()
+         .WithMessage(string.Format(ValidationMessages.Required, "Id"));
+    }
+}
