a43eb873b94d2e8c86e86899db25b34111f8a82c
adnanertorer <adnanertorer@hotmail.com>
2025-06-11 15:07:07 +0300

    CreateEmployeeCommand

---
+++ src/Adoroid.CarService.Application/Features/Employees/Commands/Create/Validators/CreateEmployeeCommandValidator.cs
@@ -0,0 +1,44 @@
+﻿using Adoroid.CarService.Application.Common.ValidationMessages;
+using FluentValidation;
+
+namespace Adoroid.CarService.Application.Features.Employees.Commands.Create.Validators;
+
+public class CreateEmployeeCommandValidator : AbstractValidator<CreateEmployeeCommand>
+{
+    public CreateEmployeeCommandValidator()
+    {
+        RuleFor(x => x.Name)
+            .NotEmpty()
+            .WithMessage(string.Format(ValidationMessages.Required, "Çalışan adı"))
+            .MaximumLength(50)
+            .WithMessage(string.Format(ValidationMessages.MaxLength, "Çalışan adı", "50"));
+
+        RuleFor(x => x.Surname)
+            .NotEmpty()
+            .WithMessage(string.Format(ValidationMessages.Required, "Çalışan soyadı"))
+            .MaximumLength(50)
+            .WithMessage(string.Format(ValidationMessages.MaxLength, "Çalışan adı", "50"));
+
+        RuleFor(x => x.PhoneNumber)
+            .NotEmpty()
+            .WithMessage(string.Format(ValidationMessages.Required, "Telefon"))
+            .MaximumLength(20)
+            .WithMessage(string.Format(ValidationMessages.MaxLength, "Telefon", "20"));
+
+        RuleFor(x => x.IsActive)
+           .NotNull()
+           .WithMessage(string.Format(ValidationMessages.Required, "Aktif/Pasif"));
+
+        RuleFor(x => x.Email)
+            .EmailAddress()
+            .WithMessage(string.Format(ValidationMessages.Required, "E-Posta"))
+            .MaximumLength(60)
+            .WithMessage(string.Format(ValidationMessages.MaxLength, "E-Posta", "60"))
+            .When(x => !string.IsNullOrEmpty(x.Email));
+
+        RuleFor(x => x.Address)
+           .MaximumLength(250)
+           .WithMessage(string.Format(ValidationMessages.MaxLength, "Adres", "250"))
+           .When(x => !string.IsNullOrEmpty(x.Address));
+    }
+}
---
+++ src/Adoroid.CarService.Application/Features/Employees/ExecptionMessages/BusinessExceptionMessages.cs
@@ -0,0 +1,7 @@
+﻿namespace Adoroid.CarService.Application.Features.Employees.ExecptionMessages;
+
+public class BusinessExceptionMessages
+{
+    public const string NotFound = "Çalışan bulunamadı.";
+    public const string AlreadyExists = "Bu çalışan zaten var.";
+}
