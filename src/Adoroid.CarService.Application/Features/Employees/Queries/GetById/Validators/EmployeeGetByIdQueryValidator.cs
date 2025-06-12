56246f44a158b257ffc02f97b547b37c2a7aff54
adnanertorer <adnanertorer@hotmail.com>
2025-06-12 13:51:23 +0300

    EmployeeGetByIdQuery

---
+++ src/Adoroid.CarService.Application/Features/Employees/Queries/GetById/Validators/EmployeeGetByIdQueryValidator.cs
@@ -0,0 +1,13 @@
+﻿using Adoroid.CarService.Application.Common.ValidationMessages;
+using FluentValidation;
+
+namespace Adoroid.CarService.Application.Features.Employees.Queries.GetById.Validators;
+
+public class EmployeeGetByIdQueryValidator : AbstractValidator<EmployeeGetByIdQuery>
+{
+    public EmployeeGetByIdQueryValidator()
+    {
+        RuleFor(x => x.Id).NotNull()
+           .WithMessage(string.Format(ValidationMessages.NotNull, "Id"));
+    }
+}
