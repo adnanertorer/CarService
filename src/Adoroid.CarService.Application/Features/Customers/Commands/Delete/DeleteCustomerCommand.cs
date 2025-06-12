d82ae8c3d3695dc6481d011af1eb5214de70e43f
adnanertorer <adnanertorer@hotmail.com>
2025-06-12 16:06:32 +0300

    DeleteCustomerCommand

---
+++ src/Adoroid.CarService.Application/Features/Customers/Commands/Delete/DeleteCustomerCommand.cs
@@ -0,0 +1,30 @@
+﻿using Adoroid.CarService.Application.Common.Abstractions.Auth;
+using Adoroid.CarService.Application.Features.Customers.ExceptionMessages;
+using Adoroid.CarService.Persistence;
+using Adoroid.Core.Application.Wrappers;
+using Microsoft.EntityFrameworkCore;
+using MinimalMediatR.Core;
+
+namespace Adoroid.CarService.Application.Features.Customers.Commands.Delete;
+
+public record DeleteCustomerCommand(Guid Id) : IRequest<Response<Guid>>;
+
+public class DeleteCustomerCommandHandler(CarServiceDbContext dbContext, ICurrentUser currentUser) : IRequestHandler<DeleteCustomerCommand, Response<Guid>>
+{
+    public async Task<Response<Guid>> Handle(DeleteCustomerCommand request, CancellationToken cancellationToken)
+    {
+        var customer = await dbContext.Customers
+             .FirstOrDefaultAsync(i => i.Id == request.Id, cancellationToken);
+
+        if (customer is null)
+            return Response<Guid>.Fail(BusinessExceptionMessages.NotFound);
+
+        customer.DeletedDate = DateTime.UtcNow;
+        customer.DeletedBy = Guid.Parse(currentUser.Id!);
+        customer.IsDeleted = true;
+
+        await dbContext.SaveChangesAsync(cancellationToken);
+
+        return Response<Guid>.Success(customer.Id);
+    }
+}
---
+++ src/Adoroid.CarService.Application/Features/Customers/Commands/Delete/Validators/DeleteCustomerCommandValidator.cs
@@ -0,0 +1,14 @@
+﻿using Adoroid.CarService.Application.Common.ValidationMessages;
+using FluentValidation;
+
+namespace Adoroid.CarService.Application.Features.Customers.Commands.Delete.Validators;
+
+public class DeleteCustomerCommandValidator : AbstractValidator<DeleteCustomerCommand>
+{
+    public DeleteCustomerCommandValidator()
+    {
+        RuleFor(x => x.Id)
+         .NotNull()
+         .WithMessage(string.Format(ValidationMessages.Required, "Id"));
+    }
+}
