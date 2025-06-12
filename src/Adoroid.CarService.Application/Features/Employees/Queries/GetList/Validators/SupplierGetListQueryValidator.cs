bfcc607bc67045fde61e7ec3371fc8bb1877bbc8
adnanertorer <adnanertorer@hotmail.com>
2025-06-12 14:40:37 +0300

    SupplierGetListQuery

---
+++ src/Adoroid.CarService.Application/Features/Employees/Queries/GetList/Validators/SupplierGetListQueryValidator.cs
@@ -0,0 +1,20 @@
+﻿using Adoroid.CarService.Application.Common.ValidationMessages;
+using Adoroid.CarService.Application.Features.Suppliers.Queries.GetList;
+using FluentValidation;
+
+namespace Adoroid.CarService.Application.Features.Employees.Queries.GetList.Validators;
+
+public class SupplierGetListQueryValidator : AbstractValidator<SupplierGetListQuery>
+{
+    public SupplierGetListQueryValidator()
+    {
+        RuleFor(i => i.PageRequest).NotNull()
+      .WithMessage(ValidationMessages.PageRequestRequired);
+
+        RuleFor(i => i.PageRequest.PageSize)
+            .GreaterThan(0)
+            .WithMessage(ValidationMessages.PageRequestPageSizeMustBeGreaterThanZero)
+            .LessThan(100)
+            .WithMessage(ValidationMessages.PageRequestPageSizeMustBeLessThan100);
+    }
+}
