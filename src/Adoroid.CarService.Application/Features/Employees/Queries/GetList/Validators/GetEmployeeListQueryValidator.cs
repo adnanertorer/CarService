3ae1c9e81974d6bbdedb85efecd7d7b9fdc141dd
adnanertorer <adnanertorer@hotmail.com>
2025-06-12 14:44:58 +0300

    fixed validator

--- src/Adoroid.CarService.Application/Features/Employees/Queries/GetList/Validators/GetEmployeeListQueryValidator.cs
+++ src/Adoroid.CarService.Application/Features/Employees/Queries/GetList/Validators/GetEmployeeListQueryValidator.cs
@@ -1,12 +1,11 @@
 ﻿using Adoroid.CarService.Application.Common.ValidationMessages;
-using Adoroid.CarService.Application.Features.Suppliers.Queries.GetList;
 using FluentValidation;
 
 namespace Adoroid.CarService.Application.Features.Employees.Queries.GetList.Validators;
 
-public class SupplierGetListQueryValidator : AbstractValidator<SupplierGetListQuery>
+public class GetEmployeeListQueryValidator : AbstractValidator<GetEmployeeListQuery>
 {
-    public SupplierGetListQueryValidator()
+    public GetEmployeeListQueryValidator()
     {
         RuleFor(i => i.PageRequest).NotNull()
       .WithMessage(ValidationMessages.PageRequestRequired);
