da9db355c8cb81ca85a8ffce5bf82538d5a10da2
adnanertorer <adnanertorer@hotmail.com>
2025-06-12 13:30:49 +0300

    DeleteEmployeeCommand

---
+++ src/Adoroid.CarService.Application/Features/Employees/Commands/Delete/DeleteEmployeeCommand.cs
@@ -0,0 +1,30 @@
+﻿using Adoroid.CarService.Application.Common.Abstractions.Auth;
+using Adoroid.CarService.Application.Features.Employees.ExecptionMessages;
+using Adoroid.CarService.Persistence;
+using Adoroid.Core.Application.Wrappers;
+using Microsoft.EntityFrameworkCore;
+using MinimalMediatR.Core;
+
+namespace Adoroid.CarService.Application.Features.Employees.Commands.Delete;
+
+public record DeleteEmployeeCommand(Guid Id) : IRequest<Response<Guid>>;
+
+public class DeleteEmployeeCommandHandler(CarServiceDbContext dbContext, ICurrentUser currentUser) : IRequestHandler<DeleteEmployeeCommand, Response<Guid>>
+{
+    public async Task<Response<Guid>> Handle(DeleteEmployeeCommand request, CancellationToken cancellationToken)
+    {
+        var employee = await dbContext.Employees.FirstOrDefaultAsync(i => i.Id == request.Id, cancellationToken);
+
+        if (employee is null)
+            return Response<Guid>.Fail(BusinessExceptionMessages.NotFound);
+
+        employee.DeletedDate = DateTime.UtcNow;
+        employee.DeletedBy = Guid.Parse(currentUser.Id!);
+        employee.IsDeleted = true;
+
+        dbContext.Employees.Update(employee);
+        await dbContext.SaveChangesAsync(cancellationToken);
+
+        return Response<Guid>.Success(employee.Id);
+    }
+}
---
+++ src/Adoroid.CarService.Application/Features/Employees/Commands/Delete/Validators/DeleteEmployeeCommandValidator.cs
@@ -0,0 +1,13 @@
+﻿using Adoroid.CarService.Application.Common.ValidationMessages;
+using FluentValidation;
+
+namespace Adoroid.CarService.Application.Features.Employees.Commands.Delete.Validators;
+
+public class DeleteEmployeeCommandValidator : AbstractValidator<DeleteEmployeeCommand>
+{
+    public DeleteEmployeeCommandValidator()
+    {
+        RuleFor(x => x.Id).NotNull()
+           .WithMessage(string.Format(ValidationMessages.NotNull, "Id"));
+    }
+}
