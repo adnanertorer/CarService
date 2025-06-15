cfa71668949a1bafa00f8834b9042d1de2f300be
adnanertorer <adnanertorer@hotmail.com>
2025-06-15 15:13:01 +0300

    GetByIdVehicleRequest

---
+++ src/Adoroid.CarService.Application/Features/Vehicles/Queries/GetById/Validators/GetByIdVehicleRequestValidators.cs
@@ -0,0 +1,14 @@
+﻿using Adoroid.CarService.Application.Common.ValidationMessages;
+using FluentValidation;
+
+namespace Adoroid.CarService.Application.Features.Vehicles.Queries.GetById.Validators;
+
+public class GetByIdVehicleRequestValidators : AbstractValidator<GetByIdVehicleRequest>
+{
+    public GetByIdVehicleRequestValidators()
+    {
+        RuleFor(x => x.Id)
+       .NotNull()
+       .WithMessage(string.Format(ValidationMessages.NotNull, "Id"));
+    }
+}
