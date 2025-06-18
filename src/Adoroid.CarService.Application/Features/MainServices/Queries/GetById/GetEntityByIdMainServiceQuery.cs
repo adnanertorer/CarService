d9e871c704adb01515fef673dcb53660c0ecbfd2
adnanertorer <adnanertorer@hotmail.com>
2025-06-18 15:17:52 +0300

    getbyid

---
+++ src/Adoroid.CarService.Application/Features/MainServices/Queries/GetById/GetEntityByIdMainServiceQuery.cs
@@ -0,0 +1,31 @@
+﻿using Adoroid.CarService.Application.Features.MainServices.Dtos;
+using Adoroid.CarService.Application.Features.MainServices.ExceptionMessages;
+using Adoroid.CarService.Application.Features.MainServices.MapperExtensions;
+using Adoroid.CarService.Persistence;
+using Adoroid.Core.Application.Wrappers;
+using Microsoft.EntityFrameworkCore;
+using MinimalMediatR.Core;
+
+namespace Adoroid.CarService.Application.Features.MainServices.Queries.GetById;
+
+public record GetEntityByIdMainServiceQuery(Guid Id) : IRequest<Response<MainServiceDto>>;
+
+public class GetEntityByIdMainServiceQueryHandler(CarServiceDbContext dbContext)
+    : IRequestHandler<GetEntityByIdMainServiceQuery, Response<MainServiceDto>>
+{
+
+    public async Task<Response<MainServiceDto>> Handle(GetEntityByIdMainServiceQuery request, CancellationToken cancellationToken)
+    {
+        var entity = await dbContext.MainServices
+            .Include(i => i.Vehicle)
+            .AsNoTracking()
+            .FirstOrDefaultAsync(e => e.Id == request.Id, cancellationToken);
+
+        if (entity is null)
+            return Response<MainServiceDto>.Fail(BusinessExceptionMessages.NotFound);
+
+        return Response<MainServiceDto>.Success(entity.FromEntity());
+    }
+}
+
+
