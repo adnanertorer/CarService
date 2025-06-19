4c8988a8d91e3d3aac394cf8f4bf1c260e8dd1db
adnanertorer <adnanertorer@hotmail.com>
2025-06-20 01:06:07 +0300

    CompanyEndpoints

---
+++ src/Adoroid.CarService.Infrastructure/CarServiceInsfrastructureServiceCollection.cs
@@ -0,0 +1,16 @@
+﻿using Adoroid.CarService.Application.Common.Abstractions.Auth;
+using Adoroid.CarService.Infrastructure.Auth;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Adoroid.CarService.Infrastructure;
+
+public static class CarServiceInsfrastructureServiceCollection
+{
+    public static IServiceCollection AddCarServiceInsfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddScoped<IAesEncryptionHelper, AesEncryptionHelper>();
+        services.AddScoped<ITokenHandler, TokenHandler>();
+        return services;
+    }
+}
