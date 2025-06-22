7934026053d9be5e44959173182109e90122a139
adnanertorer <adnanertorer@hotmail.com>
2025-06-23 02:18:43 +0300

    UpdateMasterServiceCommandValidator

---
+++ src/Adoroid.CarService.Application/Features/MasterServices/Commands/Update/Validators/UpdateMasterServiceCommandValidator.cs
@@ -0,0 +1,24 @@
+﻿using Adoroid.CarService.Application.Common.ValidationMessages;
+using FluentValidation;
+
+namespace Adoroid.CarService.Application.Features.MasterServices.Commands.Update.Validators;
+
+public class UpdateMasterServiceCommandValidator : AbstractValidator<UpdateMasterServiceCommand>
+{
+    public UpdateMasterServiceCommandValidator()
+    {
+        RuleFor(x => x.Id)
+          .NotNull()
+          .WithMessage(string.Format(ValidationMessages.Required, "Id"));
+
+        RuleFor(x => x.ServiceName)
+           .NotEmpty()
+           .WithMessage(string.Format(ValidationMessages.Required, "Servis adı"))
+           .MaximumLength(150)
+           .WithMessage(string.Format(ValidationMessages.MaxLength, "Servis adı", "150"));
+
+        RuleFor(x => x.OrderIndex)
+            .NotNull()
+            .WithMessage(string.Format(ValidationMessages.Required, "Sıralama"));
+    }
+}
