6b49040c6bcbd4677e68af39d12fef6249c8e90e
adnanertorer <adnanertorer@hotmail.com>
2025-06-23 02:16:14 +0300

    CreateMasterServiceCommandValidator

---
+++ src/Adoroid.CarService.Application/Features/MasterServices/Commands/Create/Validators/CreateMasterServiceCommandValidator.cs
@@ -0,0 +1,20 @@
+﻿using Adoroid.CarService.Application.Common.ValidationMessages;
+using FluentValidation;
+
+namespace Adoroid.CarService.Application.Features.MasterServices.Commands.Create.Validators;
+
+public class CreateMasterServiceCommandValidator : AbstractValidator<CreateMasterServiceCommand>
+{
+    public CreateMasterServiceCommandValidator()
+    {
+        RuleFor(x => x.ServiceName)
+            .NotEmpty()
+            .WithMessage(string.Format(ValidationMessages.Required, "Servis adı"))
+            .MaximumLength(150)
+            .WithMessage(string.Format(ValidationMessages.MaxLength, "Servis adı", "150"));
+
+        RuleFor(x => x.OrderIndex)
+            .NotNull()
+            .WithMessage(string.Format(ValidationMessages.Required, "Sıralama"));
+    }
+}
