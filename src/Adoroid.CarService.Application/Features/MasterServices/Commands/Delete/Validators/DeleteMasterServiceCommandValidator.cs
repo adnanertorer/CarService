298595d373397e867a43332539e8d31eb83e234a
adnanertorer <adnanertorer@hotmail.com>
2025-06-23 02:18:43 +0300

    UpdateMasterServiceCommandValidator

---
+++ src/Adoroid.CarService.Application/Features/MasterServices/Commands/Delete/Validators/DeleteMasterServiceCommandValidator.cs
@@ -0,0 +1,14 @@
+﻿using Adoroid.CarService.Application.Common.ValidationMessages;
+using FluentValidation;
+
+namespace Adoroid.CarService.Application.Features.MasterServices.Commands.Delete.Validators;
+
+public class DeleteMasterServiceCommandValidator : AbstractValidator<DeleteMasterServiceCommand>
+{
+    public DeleteMasterServiceCommandValidator()
+    {
+        RuleFor(x => x.Id)
+           .NotNull()
+           .WithMessage(string.Format(ValidationMessages.Required, "Id"));
+    }
+}
