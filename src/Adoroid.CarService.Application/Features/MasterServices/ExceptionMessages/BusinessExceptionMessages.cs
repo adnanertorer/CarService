43b9e628cf116ef7c04d5b1969b4fc7e27d77bcc
adnanertorer <adnanertorer@hotmail.com>
2025-06-23 02:08:20 +0300

    CreateMasterServiceCommand

---
+++ src/Adoroid.CarService.Application/Features/MasterServices/ExceptionMessages/BusinessExceptionMessages.cs
@@ -0,0 +1,8 @@
+﻿namespace Adoroid.CarService.Application.Features.MasterServices.ExceptionMessages;
+
+public static class BusinessExceptionMessages
+{
+    public const string NotFound = "Hizmet bulunamadı.";
+    public const string AlreadyExists = "Bu hizmet zaten var.";
+    public const string IndexAlreadyExists = "Bu sıralama numarası zaten var.";
+}
