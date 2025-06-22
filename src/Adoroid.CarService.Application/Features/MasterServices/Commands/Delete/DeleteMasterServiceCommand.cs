63159cc7253062b92530beb5f34b23caa485fa13
adnanertorer <adnanertorer@hotmail.com>
2025-06-23 02:14:01 +0300

    DeleteMasterServiceCommand

---
+++ src/Adoroid.CarService.Application/Features/MasterServices/Commands/Delete/DeleteMasterServiceCommand.cs
@@ -0,0 +1,31 @@
+﻿using Adoroid.CarService.Application.Common.Abstractions.Auth;
+using Adoroid.CarService.Application.Features.MasterServices.ExceptionMessages;
+using Adoroid.CarService.Persistence;
+using Adoroid.Core.Application.Wrappers;
+using Microsoft.EntityFrameworkCore;
+using MinimalMediatR.Core;
+
+namespace Adoroid.CarService.Application.Features.MasterServices.Commands.Delete;
+
+public record DeleteMasterServiceCommand(Guid Id) : IRequest<Response<Guid>>;
+
+public class DeleteMasterServiceCommandHandler(CarServiceDbContext dbContext, ICurrentUser currentUser)
+    : IRequestHandler<DeleteMasterServiceCommand, Response<Guid>>
+{
+    public async Task<Response<Guid>> Handle(DeleteMasterServiceCommand request, CancellationToken cancellationToken)
+    {
+        var entity = await dbContext.MasterServices.FirstOrDefaultAsync(e => e.Id == request.Id, cancellationToken);
+
+        if (entity is null)
+            return Response<Guid>.Fail(BusinessExceptionMessages.NotFound);
+
+        entity.IsDeleted = true;
+        entity.DeletedBy = Guid.Parse(currentUser.Id!);
+        entity.DeletedDate = DateTime.UtcNow;
+
+        await dbContext.SaveChangesAsync(cancellationToken);
+
+        return Response<Guid>.Success(entity.Id);
+    }
+}
+
