eed28048978a4ddfb00127b590214d65c0086de4
adnanertorer <adnanertorer@hotmail.com>
2025-06-23 02:22:04 +0300

    GetByIdMasterServiceQueryValidator

---
+++ src/Adoroid.CarService.Application/Features/MasterServices/Queries/GetById/Validators/GetByIdMasterServiceQueryValidator.cs
@@ -0,0 +1,14 @@
+﻿using Adoroid.CarService.Application.Common.ValidationMessages;
+using FluentValidation;
+
+namespace Adoroid.CarService.Application.Features.MasterServices.Queries.GetById.Validators;
+
+public class GetByIdMasterServiceQueryValidator : AbstractValidator<GetByIdMasterServiceQuery>
+{
+    public GetByIdMasterServiceQueryValidator()
+    {
+        RuleFor(x => x.Id)
+          .NotNull()
+          .WithMessage(string.Format(ValidationMessages.Required, "Id"));
+    }
+}
