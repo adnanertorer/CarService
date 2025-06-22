77009d8b9eacbb70c91b57a604e84306cdd602a9
adnanertorer <adnanertorer@hotmail.com>
2025-06-23 01:48:51 +0300

    MasterService, CompanyService

---
+++ src/Adoroid.CarService.Persistence/EntityConfiguration/CompanyServiceConfiguration.cs
@@ -0,0 +1,33 @@
+﻿using Adoroid.CarService.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Adoroid.CarService.Persistence.EntityConfiguration;
+
+public class CompanyServiceConfiguration : IEntityTypeConfiguration<CompanyService>
+{
+    public void Configure(EntityTypeBuilder<CompanyService> builder)
+    {
+        builder.HasKey(b => b.Id);
+        builder.Property(b => b.CompanyId).IsRequired();
+        builder.Property(b => b.MasterServiceId).IsRequired();
+
+        builder.Property(i => i.CreatedDate).IsRequired();
+        builder.Property(c => c.CreatedBy).IsRequired().HasMaxLength(64);
+
+        builder.Property(c => c.UpdatedBy).HasMaxLength(64);
+        builder.Property(c => c.DeletedBy).HasMaxLength(64);
+
+        builder.HasQueryFilter(c => c.IsDeleted == false || c.DeletedDate == null);
+
+        builder.HasOne(b => b.Company)
+          .WithMany(c => c.CompanyServices)
+          .HasForeignKey(b => b.CompanyId)
+          .OnDelete(DeleteBehavior.NoAction);
+
+        builder.HasOne(b => b.MasterService)
+          .WithMany(c => c.CompanyServices)
+          .HasForeignKey(b => b.CompanyId)
+          .OnDelete(DeleteBehavior.NoAction);
+    }
+}
---
+++ src/Adoroid.CarService.Persistence/EntityConfiguration/MasterServiceConfiguration.cs
@@ -0,0 +1,130 @@
+﻿using Adoroid.CarService.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Adoroid.CarService.Persistence.EntityConfiguration;
+
+public class MasterServiceConfiguration : IEntityTypeConfiguration<MasterService>
+{
+    public void Configure(EntityTypeBuilder<MasterService> builder)
+    {
+        builder.HasKey(b => b.Id);
+        builder.Property(b => b.ServiceName).IsRequired().HasMaxLength(150);
+        builder.Property(b => b.OrderIndex).IsRequired();
+
+        builder.Property(i => i.CreatedDate).IsRequired();
+        builder.Property(c => c.CreatedBy).IsRequired().HasMaxLength(64);
+
+        builder.Property(c => c.UpdatedBy).HasMaxLength(64);
+        builder.Property(c => c.DeletedBy).HasMaxLength(64);
+
+        builder.HasQueryFilter(c => c.IsDeleted == false || c.DeletedDate == null);
+
+        builder.HasData(new List<MasterService> {
+            new()
+            {
+                CreatedBy = Guid.NewGuid(),
+                CreatedDate = DateTime.UtcNow,
+                IsDeleted = false,
+                OrderIndex = 0,
+                ServiceName = "Motor Tamir/Bakım"
+            },
+            new()
+            {
+                CreatedBy = Guid.NewGuid(),
+                CreatedDate = DateTime.UtcNow,
+                IsDeleted = false,
+                OrderIndex = 1,
+                ServiceName = "Oto Elektrik"
+            },
+            new()
+            {
+                CreatedBy = Guid.NewGuid(),
+                CreatedDate = DateTime.UtcNow,
+                IsDeleted = false,
+                OrderIndex = 2,
+                ServiceName = "Kaporta/Boya"
+            },
+            new()
+            {
+                CreatedBy = Guid.NewGuid(),
+                CreatedDate = DateTime.UtcNow,
+                IsDeleted = false,
+                OrderIndex = 3,
+                ServiceName = "Isıtma/Klima"
+            },
+             new()
+            {
+                CreatedBy = Guid.NewGuid(),
+                CreatedDate = DateTime.UtcNow,
+                IsDeleted = false,
+                OrderIndex = 4,
+                ServiceName = "Lastik Tamir/Değişim"
+            },
+             new()
+            {
+                CreatedBy = Guid.NewGuid(),
+                CreatedDate = DateTime.UtcNow,
+                IsDeleted = false,
+                OrderIndex = 5,
+                ServiceName = "Oto Cam"
+            },
+              new()
+            {
+                CreatedBy = Guid.NewGuid(),
+                CreatedDate = DateTime.UtcNow,
+                IsDeleted = false,
+                OrderIndex = 6,
+                ServiceName = "Egzoz"
+            },
+            new()
+            {
+                CreatedBy = Guid.NewGuid(),
+                CreatedDate = DateTime.UtcNow,
+                IsDeleted = false,
+                OrderIndex = 7,
+                ServiceName = "Elektronik"
+            },
+            new()
+            {
+                CreatedBy = Guid.NewGuid(),
+                CreatedDate = DateTime.UtcNow,
+                IsDeleted = false,
+                OrderIndex = 8,
+                ServiceName = "Lpg Montaj/Dönüşüm"
+            },
+            new()
+            {
+                CreatedBy = Guid.NewGuid(),
+                CreatedDate = DateTime.UtcNow,
+                IsDeleted = false,
+                OrderIndex = 9,
+                ServiceName = "Ekspertiz"
+            },
+            new()
+            {
+                CreatedBy = Guid.NewGuid(),
+                CreatedDate = DateTime.UtcNow,
+                IsDeleted = false,
+                OrderIndex = 10,
+                ServiceName = "Oto Döşeme"
+            },
+            new()
+            {
+                CreatedBy = Guid.NewGuid(),
+                CreatedDate = DateTime.UtcNow,
+                IsDeleted = false,
+                OrderIndex = 11,
+                ServiceName = "Oto Modifiye"
+            },
+            new()
+            {
+                CreatedBy = Guid.NewGuid(),
+                CreatedDate = DateTime.UtcNow,
+                IsDeleted = false,
+                OrderIndex = 12,
+                ServiceName = "Oto Temizlik"
+            }
+        });
+    }
+}
