e838deab7592bb7974ab81301c78d320ccac8d85
adnanertorer <adnanertorer@hotmail.com>
2025-06-23 03:39:16 +0300

    GetListCompanyServiceQueryValidator

---
+++ src/Adoroid.CarService.Application/Features/CompanyMasterServices/Queries/GetList/Validators/GetListCompanyServiceQueryValidator.cs
@@ -0,0 +1,19 @@
+﻿using Adoroid.CarService.Application.Common.ValidationMessages;
+using FluentValidation;
+
+namespace Adoroid.CarService.Application.Features.CompanyMasterServices.Queries.GetList.Validators;
+
+public class GetListCompanyServiceQueryValidator : AbstractValidator<GetListCompanyServiceQuery>
+{
+    public GetListCompanyServiceQueryValidator()
+    {
+        RuleFor(i => i.PageRequest).NotNull()
+      .WithMessage(ValidationMessages.PageRequestRequired);
+
+        RuleFor(i => i.PageRequest.PageSize)
+            .GreaterThan(0)
+            .WithMessage(ValidationMessages.PageRequestPageSizeMustBeGreaterThanZero)
+            .LessThan(100)
+            .WithMessage(ValidationMessages.PageRequestPageSizeMustBeLessThan100);
+    }
+}
