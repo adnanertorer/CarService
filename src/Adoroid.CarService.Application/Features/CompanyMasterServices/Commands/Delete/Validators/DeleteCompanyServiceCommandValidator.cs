40d0cc1fdfc5896f32cf0086b6fd1392eb99aef4
adnanertorer <adnanertorer@hotmail.com>
2025-06-23 03:27:48 +0300

    DeleteCompanyServiceCommandValidator

---
+++ src/Adoroid.CarService.Application/Features/CompanyMasterServices/Commands/Delete/Validators/DeleteCompanyServiceCommandValidator.cs
@@ -0,0 +1,14 @@
+﻿using Adoroid.CarService.Application.Common.ValidationMessages;
+using FluentValidation;
+
+namespace Adoroid.CarService.Application.Features.CompanyMasterServices.Commands.Delete.Validators;
+
+public class DeleteCompanyServiceCommandValidator : AbstractValidator<DeleteCompanyServiceCommand>
+{
+    public DeleteCompanyServiceCommandValidator()
+    {
+        RuleFor(x => x.Id)
+          .NotNull()
+          .WithMessage(string.Format(ValidationMessages.Required, "Id"));
+    }
+}
