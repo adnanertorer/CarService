b73255d2576b232744f7df9ecce52f296157b2f1
adnanertorer <adnanertorer@hotmail.com>
2025-06-23 03:12:25 +0300

    CompanyServiceMappingExtensions

---
+++ src/Adoroid.CarService.Application/Features/CompanyMasterServices/Dtos/MasterServiceDto.cs
@@ -0,0 +1,7 @@
+﻿namespace Adoroid.CarService.Application.Features.CompanyMasterServices.Dtos;
+
+public class MasterServiceDto
+{
+    public Guid Id { get; set; }
+    public string ServiceName { get; set; }
+}
