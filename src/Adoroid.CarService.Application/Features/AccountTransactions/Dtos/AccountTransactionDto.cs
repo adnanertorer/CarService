9917da44d112a53c7406ce6651afbc06dd71c45b
adnanertorer <adnanertorer@hotmail.com>
2025-06-23 19:42:51 +0300

    CreateClaimCommand

---
+++ src/Adoroid.CarService.Application/Features/AccountTransactions/Dtos/AccountTransactionDto.cs
@@ -0,0 +1,13 @@
+﻿namespace Adoroid.CarService.Application.Features.AccountTransactions.Dtos;
+
+public class AccountTransactionDto
+{
+    public Guid CustomerId { get; set; }
+    public int TransactionType { get; set; } // 0: Income, 1: Expense
+    public decimal Claim { get; set; } // Claim amount for expenses
+    public decimal Debt { get; set; } // Debt amount for incomes
+    public decimal Balance { get; set; } // Balance after the transaction
+    public DateTime TransactionDate { get; set; } // Date of the transaction
+
+    public CustomerDto? Customer { get; set; }
+}
