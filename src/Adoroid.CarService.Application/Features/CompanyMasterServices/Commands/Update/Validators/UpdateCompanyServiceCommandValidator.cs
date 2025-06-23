00cf78465b5665ec89c1fd10b16948cb586b5b5b
adnanertorer <adnanertorer@hotmail.com>
2025-06-23 03:26:48 +0300

    UpdateCompanyServiceCommandValidator

---
+++ src/Adoroid.CarService.Application/Features/CompanyMasterServices/Commands/Update/Validators/UpdateCompanyServiceCommandValidator.cs
@@ -0,0 +1,18 @@
+﻿using Adoroid.CarService.Application.Common.ValidationMessages;
+using FluentValidation;
+
+namespace Adoroid.CarService.Application.Features.CompanyMasterServices.Commands.Update.Validators;
+
+public class UpdateCompanyServiceCommandValidator : AbstractValidator<UpdateCompanyServiceCommand>
+{
+    public UpdateCompanyServiceCommandValidator()
+    {
+        RuleFor(x => x.Id)
+           .NotNull()
+           .WithMessage(string.Format(ValidationMessages.Required, "Id"));
+
+        RuleFor(x => x.MasterServiceId)
+           .NotNull()
+           .WithMessage(string.Format(ValidationMessages.Required, "Hizmet id"));
+    }
+}
