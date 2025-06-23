33338064058a2225c5632a62f834b4bd3b8a39c2
adnanertorer <adnanertorer@hotmail.com>
2025-06-23 03:12:25 +0300

    CompanyServiceMappingExtensions

---
+++ src/Adoroid.CarService.Application/Features/CompanyMasterServices/Dtos/CompanyDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Adoroid.CarService.Application.Features.CompanyMasterServices.Dtos;
+
+public class CompanyDto
+{
+    public Guid Id { get; set; }
+    public string CompanyName { get; set; }
+    public string AuthorizedName { get; set; }
+    public string AuthorizedSurname { get; set; }
+    public string CompanyPhone { get; set; }
+}
---
+++ src/Adoroid.CarService.Application/Features/CompanyMasterServices/MapperExtensions/CompanyServiceMappingExtensions.cs
@@ -0,0 +1,30 @@
+﻿using Adoroid.CarService.Application.Features.CompanyMasterServices.Dtos;
+using Adoroid.CarService.Domain.Entities;
+
+namespace Adoroid.CarService.Application.Features.CompanyMasterServices.MapperExtensions;
+
+public static class CompanyServiceMappingExtensions
+{
+    public static CompanyServiceDto FromEntity(this CompanyService companyService)
+    {
+        return new CompanyServiceDto
+        {
+            Company = companyService.Company != null ? new CompanyDto
+            {
+                AuthorizedName = companyService.Company.AuthorizedName,
+                AuthorizedSurname = companyService.Company.AuthorizedSurname,
+                CompanyName = companyService.Company.CompanyName,
+                CompanyPhone = companyService.Company.CompanyPhone,
+                Id = companyService.Company.Id
+            } : null,
+            CompanyId = companyService.CompanyId,
+            Id = companyService.Id,
+            MasterService = companyService.MasterService != null ? new MasterServiceDto
+            {
+                Id = companyService.MasterService.Id,
+                ServiceName = companyService.MasterService.ServiceName
+            } : null,
+            MasterServiceId = companyService.MasterServiceId
+        };
+    }
+}
