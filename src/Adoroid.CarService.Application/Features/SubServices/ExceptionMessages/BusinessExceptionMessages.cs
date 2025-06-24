ac5c4ecbef5a6bb01d1f2b1031b491ffcfbc1902
adnanertorer <adnanertorer@hotmail.com>
2025-06-25 00:31:46 +0300

    CreateSubServiceCommandHandler

--- src/Adoroid.CarService.Application/Features/SubServices/ExceptionMessages/BusinessExceptionMessages.cs
+++ src/Adoroid.CarService.Application/Features/SubServices/ExceptionMessages/BusinessExceptionMessages.cs
@@ -5,4 +5,5 @@ public class BusinessExceptionMessages
     public const string NotFound = "Servis bulunamadı.";
     public const string AlreadyExists = "Bu servis zaten var.";
     public const string MainServiceNotFound = "Bu işlem için servis açılmamış";
+    public const string EmployeeNotFound = "Çalışan bulunamadı";
 }
