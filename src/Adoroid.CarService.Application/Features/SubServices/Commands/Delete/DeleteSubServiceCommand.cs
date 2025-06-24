c75c5e1d90fa3e2bcd7861234d5c3abf6dac1f9e
adnanertorer <adnanertorer@hotmail.com>
2025-06-25 00:33:13 +0300

    DeleteSubServiceCommand

--- src/Adoroid.CarService.Application/Features/SubServices/Commands/Delete/DeleteSubServiceCommand.cs
+++ src/Adoroid.CarService.Application/Features/SubServices/Commands/Delete/DeleteSubServiceCommand.cs
@@ -1,4 +1,6 @@
 ﻿using Adoroid.CarService.Application.Common.Abstractions.Auth;
+using Adoroid.CarService.Application.Common.Abstractions.Caching;
+using Adoroid.CarService.Application.Common.Extensions;
 using Adoroid.CarService.Application.Features.SubServices.ExceptionMessages;
 using Adoroid.CarService.Persistence;
 using Adoroid.Core.Application.Wrappers;
@@ -9,7 +11,7 @@ namespace Adoroid.CarService.Application.Features.SubServices.Commands.Delete;
 
 public record DeleteSubServiceCommand(Guid Id) : IRequest<Response<Guid>>;
 
-public class DeleteSubServiceCommandHandler(CarServiceDbContext dbContext, ICurrentUser currentUser)
+public class DeleteSubServiceCommandHandler(CarServiceDbContext dbContext, ICurrentUser currentUser, ICacheService cacheService)
     : IRequestHandler<DeleteSubServiceCommand, Response<Guid>>
 {
     public async Task<Response<Guid>> Handle(DeleteSubServiceCommand request, CancellationToken cancellationToken)
@@ -24,6 +26,9 @@ public class DeleteSubServiceCommandHandler(CarServiceDbContext dbContext, ICurr
         entity.DeletedDate = DateTime.UtcNow;
 
         await dbContext.SaveChangesAsync(cancellationToken);
+
+        var cacheListKey = $"subservices:list:{currentUser.CompanyId!}";
+        await cacheService.RemoveFromListAsync<dynamic>(cacheListKey, request.Id.ToString());
 
         return Response<Guid>.Success(entity.Id);
     }
