d52c0c75c17e9c378f604699ae5e43447a3a5035
adnanertorer <adnanertorer@hotmail.com>
2025-06-24 12:28:22 +0300

    DeleteClaimCommandValidator

---
+++ src/Adoroid.CarService.Application/Features/AccountTransactions/Commands/Delete/Validators/DeleteClaimCommandValidator.cs
@@ -0,0 +1,16 @@
+﻿using Adoroid.CarService.Application.Common.ValidationMessages;
+using FluentValidation;
+
+namespace Adoroid.CarService.Application.Features.AccountTransactions.Commands.Delete.Validators;
+
+public class DeleteClaimCommandValidator : AbstractValidator<DeleteClaimCommand>
+{
+    public DeleteClaimCommandValidator()
+    {
+        RuleFor(x => x.Id)
+          .NotNull()
+          .WithMessage(string.Format(ValidationMessages.Required, "Id"));
+    }
+}
+
+
