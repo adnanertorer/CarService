40ca6328c047d3309923f990e096b8433d732c1f
adnanertorer <adnanertorer@hotmail.com>
2025-06-24 12:31:21 +0300

    GetByIdAccountTransactionRequest

---
+++ src/Adoroid.CarService.Application/Features/AccountTransactions/Queries/GetById/GetByIdAccountTransactionRequest.cs
@@ -0,0 +1,30 @@
+﻿using Adoroid.CarService.Application.Features.AccountTransactions.Dtos;
+using Adoroid.CarService.Application.Features.AccountTransactions.ExceptionMessages;
+using Adoroid.CarService.Application.Features.AccountTransactions.MapperExtensions;
+using Adoroid.CarService.Persistence;
+using Adoroid.Core.Application.Wrappers;
+using Microsoft.EntityFrameworkCore;
+using MinimalMediatR.Core;
+
+namespace Adoroid.CarService.Application.Features.AccountTransactions.Queries.GetById;
+
+public record GetByIdAccountTransactionRequest(Guid Id) : IRequest<Response<AccountTransactionDto>>;
+
+public class GetByIdAccountTransactionRequestHandler(CarServiceDbContext dbContext)
+    : IRequestHandler<GetByIdAccountTransactionRequest, Response<AccountTransactionDto>>
+{
+
+    public async Task<Response<AccountTransactionDto>> Handle(GetByIdAccountTransactionRequest request, CancellationToken cancellationToken)
+    {
+        var entity = await dbContext.AccountingTransactions
+            .Include(i => i.Customer)
+            .AsNoTracking()
+            .FirstOrDefaultAsync(e => e.Id == request.Id, cancellationToken);
+
+        if (entity is null)
+            return Response<AccountTransactionDto>.Fail(BusinessExceptionMessages.NotFound);
+
+        return Response<AccountTransactionDto>.Success(entity.FromEntity());
+    }
+}
+
