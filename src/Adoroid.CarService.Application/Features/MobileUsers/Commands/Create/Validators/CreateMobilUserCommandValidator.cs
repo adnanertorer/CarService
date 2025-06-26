1e7e38e782d9131bafcf4dcfefc397023329c07c
adnanertorer <adnanertorer@hotmail.com>
2025-06-26 16:34:58 +0300

    CreateMobilUserCommandValidator

---
+++ src/Adoroid.CarService.Application/Features/MobileUsers/Commands/Create/Validators/CreateMobilUserCommandValidator.cs
@@ -0,0 +1,42 @@
+﻿using Adoroid.CarService.Application.Common.ValidationMessages;
+using FluentValidation;
+
+namespace Adoroid.CarService.Application.Features.MobileUsers.Commands.Create.Validators;
+
+public class CreateMobilUserCommandValidator : AbstractValidator<CreateMobilUserCommand>
+{
+    public CreateMobilUserCommandValidator()
+    {
+        RuleFor(x => x.Email)
+               .NotEmpty()
+               .WithMessage(string.Format(ValidationMessages.Required, "E-Posta"))
+               .EmailAddress()
+               .WithMessage(string.Format(ValidationMessages.Email, "E-Posta"));
+
+        RuleFor(x => x.Name)
+             .NotEmpty()
+             .WithMessage(string.Format(ValidationMessages.Required, "Adı"))
+             .MaximumLength(50)
+             .WithMessage(string.Format(ValidationMessages.MaxLength, "Adı", "50"));
+
+        RuleFor(x => x.Surname)
+             .NotEmpty()
+             .WithMessage(string.Format(ValidationMessages.Required, "Soyadı"))
+             .MaximumLength(50)
+             .WithMessage(string.Format(ValidationMessages.MaxLength, "Soyadı", "50"));
+
+        RuleFor(x => x.Password)
+             .NotEmpty()
+             .WithMessage(string.Format(ValidationMessages.Required, "Parola"))
+             .MinimumLength(8)
+             .WithMessage(string.Format(ValidationMessages.MaxLength, "Parola", "8"));
+
+        RuleFor(x => x.PhoneNumber)
+             .NotEmpty()
+             .WithMessage(string.Format(ValidationMessages.Required, "Telefon"))
+             .MinimumLength(10)
+             .WithMessage(string.Format(ValidationMessages.MaxLength, "Telefon", "10"))
+             .MaximumLength(16)
+             .WithMessage(string.Format(ValidationMessages.MaxLength, "Telefon", "16"));
+    }
+}
