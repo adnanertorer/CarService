b85ad6e8f830ca9c8c5b9500eee49e04962c4c78
adnanertorer <adnanertorer@hotmail.com>
2025-06-28 11:03:08 +0300

    added city and districts

---
+++ src/Adoroid.CarService.Persistence/Migrations/20250627193542_Seed_Cities_And_Districts.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Adoroid.CarService.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class Seed_Cities_And_Districts : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            var sql = File.ReadAllText("Migrations/Scripts/SeedCitiesAndDistricts.sql");
+            migrationBuilder.Sql(sql);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
