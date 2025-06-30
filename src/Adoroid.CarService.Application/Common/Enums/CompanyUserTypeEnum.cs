70bdb25d768cd06d855596971081b4fef371afc9
adnanertorer <adnanertorer@hotmail.com>
2025-06-30 17:24:47 +0300

    UserToCompanyDto, CompanyDto, CompanyUserTypeEnum

---
+++ src/Adoroid.CarService.Application/Common/Enums/CompanyUserTypeEnum.cs
@@ -0,0 +1,11 @@
+﻿,using System.ComponentModel;
+
+namespace Adoroid.CarService.Application.Common.Enums;
+
+public enum CompanyUserTypeEnum
+{
+    [Description("Master User")]
+    Master = 1,
+    [Description("Employee User")]
+    Employee = 2
+}
