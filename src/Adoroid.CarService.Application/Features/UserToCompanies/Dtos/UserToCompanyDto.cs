1f363aedd9362f61d7daa7d7c9bb2dfad88f3f24
adnanertorer <adnanertorer@hotmail.com>
2025-06-30 17:25:57 +0300

    features

--- src/Adoroid.CarService.Application/Features/UserToCompanies/Dtos/UserToCompanyDto.cs
+++ src/Adoroid.CarService.Application/Features/UserToCompanies/Dtos/UserToCompanyDto.cs
@@ -1,9 +1,11 @@
-﻿namespace Adoroid.CarService.Application.Features.UserToCompanies.Dtos;
+﻿using Adoroid.CarService.Application.Common.Enums;
+
+namespace Adoroid.CarService.Application.Features.UserToCompanies.Dtos;
 
 public class UserToCompanyDto
 {
     public Guid Id { get; set; }
     public Guid UserId { get; set; }
     public Guid CompanyId { get; set; }
-    public int UserType { get; set; }
+    public CompanyUserTypeEnum UserType { get; set; }
 }
