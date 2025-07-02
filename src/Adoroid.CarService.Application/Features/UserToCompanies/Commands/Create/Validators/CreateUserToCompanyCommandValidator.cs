a97b8599e35524c234958e1ea61834c59bd77140
adnanertorer <adnanertorer@hotmail.com>
2025-07-02 15:20:47 +0300

    UpdateUserToCompanyCommand

---
+++ src/Adoroid.CarService.Application/Features/UserToCompanies/Commands/Create/Validators/CreateUserToCompanyCommandValidator.cs
@@ -0,0 +1,18 @@
+﻿using Adoroid.CarService.Application.Common.ValidationMessages;
+using FluentValidation;
+
+namespace Adoroid.CarService.Application.Features.UserToCompanies.Commands.Create.Validators;
+
+public class CreateUserToCompanyCommandValidator : AbstractValidator<CreateUserToCompanyCommand>
+{
+    public CreateUserToCompanyCommandValidator()
+    {
+        RuleFor(x => x.CompanyUserType)
+          .NotNull()
+          .WithMessage(string.Format(ValidationMessages.Required, "Kullanıcı tipi"));
+
+        RuleFor(x => x.CompanyId)
+          .NotNull()
+          .WithMessage(string.Format(ValidationMessages.Required, "Firma Id"));
+    }
+}
