32cc6afb7ebea6bf44181022a921ee3910853cbf
adnanertorer <adnanertorer@hotmail.com>
2025-07-02 13:20:04 +0300

    CreateUserToCompanyCommand

---
+++ src/Adoroid.CarService.Application/Features/UserToCompanies/ExceptionMessages/BusinessExceptionMessages.cs
@@ -0,0 +1,8 @@
+﻿namespace Adoroid.CarService.Application.Features.UserToCompanies.ExceptionMessages;
+
+public static class BusinessExceptionMessages
+{
+    public const string CompanyNotFound = "Firma bulunamadı.";
+    public const string UserNotFound = "Kullanıcı bulunamadı.";
+    public const string AlreadyExists = "Bu kayıt zaten var.";
+}
