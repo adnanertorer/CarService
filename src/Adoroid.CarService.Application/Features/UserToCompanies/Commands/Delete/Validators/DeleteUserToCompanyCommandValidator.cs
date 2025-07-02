d4307495c703ae9a604347421e07aa2719a64a0d
adnanertorer <adnanertorer@hotmail.com>
2025-07-02 15:25:31 +0300

    DeleteUserToCompanyCommand

---
+++ src/Adoroid.CarService.Application/Features/UserToCompanies/Commands/Delete/Validators/DeleteUserToCompanyCommandValidator.cs
@@ -0,0 +1,14 @@
+﻿using Adoroid.CarService.Application.Common.ValidationMessages;
+using FluentValidation;
+
+namespace Adoroid.CarService.Application.Features.UserToCompanies.Commands.Delete.Validators;
+
+public class DeleteUserToCompanyCommandValidator : AbstractValidator<DeleteUserToCompanyCommand>
+{
+    public DeleteUserToCompanyCommandValidator()
+    {
+        RuleFor(x => x.Id)
+           .NotNull()
+           .WithMessage(string.Format(ValidationMessages.Required, "Id"));
+    }
+}
