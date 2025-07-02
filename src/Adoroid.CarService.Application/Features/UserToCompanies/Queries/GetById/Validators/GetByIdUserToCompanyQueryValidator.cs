318eee70b2ec56007dfb916e5eff97cdeb4cf822
adnanertorer <adnanertorer@hotmail.com>
2025-07-02 15:28:31 +0300

    GetByIdUserToCompanyQuery

---
+++ src/Adoroid.CarService.Application/Features/UserToCompanies/Queries/GetById/Validators/GetByIdUserToCompanyQueryValidator.cs
@@ -0,0 +1,14 @@
+﻿using Adoroid.CarService.Application.Common.ValidationMessages;
+using FluentValidation;
+
+namespace Adoroid.CarService.Application.Features.UserToCompanies.Queries.GetById.Validators;
+
+public class GetByIdUserToCompanyQueryValidator : AbstractValidator<GetByIdUserToCompanyQuery>
+{
+    public GetByIdUserToCompanyQueryValidator()
+    {
+        RuleFor(x => x.Id)
+            .NotNull()
+            .WithMessage(string.Format(ValidationMessages.Required, "Id"));
+    }
+}
