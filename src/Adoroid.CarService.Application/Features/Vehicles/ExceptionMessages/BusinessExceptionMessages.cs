a7e6c4fefa9bd093e0d8f1955a0418ca01bffe6a
adnanertorer <adnanertorer@hotmail.com>
2025-07-04 13:57:02 +0300

    AssingVehicleToUserCommand

--- src/Adoroid.CarService.Application/Features/Vehicles/ExceptionMessages/BusinessExceptionMessages.cs
+++ src/Adoroid.CarService.Application/Features/Vehicles/ExceptionMessages/BusinessExceptionMessages.cs
@@ -5,4 +5,5 @@ public class BusinessExceptionMessages
     public const string NotFound = "Araç bulunamadı.";
     public const string AlreadyExists = "Bu araç zaten var.";
     public const string VehicleHasMainServices = "Bu araç üzerinde servis kayıtları olduğu için sizin tarafınızdan silinemez.Servis kayıt işlemlerinin silinmesi için ustayı bilgilendirin.";
+    public const string VehicleUserRecordNotFound = "Araç kullanıcı kaydı bulunamadı.";
 }
