2db449f1cdd3bfc7619f18bd60d7100410ace849
adnanertorer <adnanertorer@hotmail.com>
2025-07-04 10:15:02 +0300

    GetBySerialNumberQuery

---
+++ src/Adoroid.CarService.Application/Features/Vehicles/Queries/GetBySerialNumber/Validators/GetBySerialNumberQueryValidator.cs
@@ -0,0 +1,18 @@
+﻿using Adoroid.CarService.Application.Common.ValidationMessages;
+using FluentValidation;
+
+namespace Adoroid.CarService.Application.Features.Vehicles.Queries.GetBySerialNumber.Validators;
+
+public class GetBySerialNumberQueryValidator : AbstractValidator<GetBySerialNumberQuery>
+{
+    public GetBySerialNumberQueryValidator()
+    {
+        RuleFor(i => i.SerialNumber)
+            .NotEmpty()
+            .WithMessage(string.Format(ValidationMessages.Required, "Şase No"));
+
+        RuleFor(i => i.PlateNumber)
+            .NotEmpty()
+            .WithMessage(string.Format(ValidationMessages.Required, "Plaka"));
+    }
+}
