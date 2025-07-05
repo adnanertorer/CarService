ceaddd044b859ba116261244e40512e53b17219d
adnanertorer <adnanertorer@hotmail.com>
2025-07-05 17:21:35 +0300

    AdjustmentAccountTransactionCommand

---
+++ src/Adoroid.CarService.Persistence/Migrations/20250705130942_alter_mainserviceid_to_accounttransaction.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Adoroid.CarService.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class alter_mainserviceid_to_accounttransaction : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "MainServiceId",
+                table: "AccountingTransactions",
+                type: "uuid",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MainServiceId",
+                table: "AccountingTransactions");
+        }
+    }
+}
