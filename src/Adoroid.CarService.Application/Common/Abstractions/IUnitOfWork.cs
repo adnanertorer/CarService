19ee9a287eaebabbfb900b9d0bb74cf5ab3d4449
adnanertorer <adnanertorer@hotmail.com>
2025-07-14 03:20:17 +0300

    added userepository

---
+++ src/Adoroid.CarService.Application/Common/Abstractions/IUnitOfWork.cs
@@ -0,0 +1,10 @@
+﻿using Adoroid.CarService.Application.Features.Users.Abtracts.Repositories;
+
+namespace Adoroid.CarService.Application.Common.Abstractions;
+
+public interface IUnitOfWork
+{
+    IUserRepository Users { get; }
+
+    Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+}
