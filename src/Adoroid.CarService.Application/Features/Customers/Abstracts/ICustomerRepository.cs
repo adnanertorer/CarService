f0f55955038d9c9a0a008223b16525175d9f88de
adnanertorer <adnanertorer@hotmail.com>
2025-07-14 16:32:42 +0300

    added customerrepository

---
+++ src/Adoroid.CarService.Application/Features/Customers/Abstracts/ICustomerRepository.cs
@@ -0,0 +1,21 @@
+﻿using Adoroid.CarService.Domain.Entities;
+
+namespace Adoroid.CarService.Application.Features.Customers.Abstracts;
+
+public interface ICustomerRepository
+{
+    Task<bool> IsCustomerExistsAsync(string name, string surname, Guid companyId, string phone,
+        CancellationToken cancellationToken);
+    Task<Customer?> GetByIdAsync(Guid customerId, bool asNoTracking = true, 
+        CancellationToken cancellationToken);
+
+    Task<Customer?> GetByMobileUserIdAsync(Guid mobileUserId, bool asNoTracking = true,
+        CancellationToken cancellationToken);
+
+    Task AddAsync(Customer customer, CancellationToken cancellationToken);
+
+    Task<(Customer Customer, List<(VehicleUser VehicleUser, Vehicle Vehicle)> VehicleUsers)> 
+        GetWithVehicleUsersAsync(Guid customerId, CancellationToken cancellationToken);
+
+    IQueryable<Customer> GetAllWithIncludes(Guid companyId);
+}
--- src/Adoroid.CarService.Application/Features/Customers/Commands/Update/UpdateCustomerCommand.cs
+++ src/Adoroid.CarService.Application/Features/Customers/Commands/Update/UpdateCustomerCommand.cs
@@ -1,10 +1,9 @@
-﻿using Adoroid.CarService.Application.Common.Abstractions.Auth;
+﻿using Adoroid.CarService.Application.Common.Abstractions;
+using Adoroid.CarService.Application.Common.Abstractions.Auth;
 using Adoroid.CarService.Application.Features.Customers.Dtos;
 using Adoroid.CarService.Application.Features.Customers.ExceptionMessages;
 using Adoroid.CarService.Application.Features.Customers.MapperExtensions;
-using Adoroid.CarService.Persistence;
 using Adoroid.Core.Application.Wrappers;
-using Microsoft.EntityFrameworkCore;
 using MinimalMediatR.Core;
 
 namespace Adoroid.CarService.Application.Features.Customers.Commands.Update;
@@ -12,26 +11,14 @@ namespace Adoroid.CarService.Application.Features.Customers.Commands.Update;
 public record UpdateCustomerCommand(Guid Id, string Name, string Surname, string? Email, string Phone, string? Address,
     string? TaxNumber, string? TaxOffice, bool IsActive, int CityId, int DistrictId) : IRequest<Response<CustomerDto>>;
 
-public class UpdateCustomerCommandHandler(CarServiceDbContext dbContext, ICurrentUser currentUser) : IRequestHandler<UpdateCustomerCommand,
+public class UpdateCustomerCommandHandler(IUnitOfWork unitOfWork, ICurrentUser currentUser) : IRequestHandler<UpdateCustomerCommand,
     Response<CustomerDto>>
 {
     public async Task<Response<CustomerDto>> Handle(UpdateCustomerCommand request, CancellationToken cancellationToken)
     {
-        var customer = await dbContext.Customers
-            .Select(i => new
-            {
-                Customer = i,
-                VehicleUsers = i.VehicleUsers
-                     .Where(j => (i.MobileUserId != null && j.UserId == i.MobileUserId) || j.UserId == i.Id)
-                     .Select(vu => new
-                     {
-                         VehicleUser = vu,
-                         Vehicle = vu.Vehicle
-                     })
-            })
-            .FirstOrDefaultAsync(i => i.Customer.Id == request.Id, cancellationToken);
+        var customer = await unitOfWork.Customers.GetWithVehicleUsersAsync(request.Id, cancellationToken);
 
-        if (customer is null)
+        if (customer.Customer is null)
             return Response<CustomerDto>.Fail(BusinessExceptionMessages.NotFound);
 
         customer.Customer.Surname = request.Surname;
@@ -48,7 +35,7 @@ public class UpdateCustomerCommandHandler(CarServiceDbContext dbContext, ICurren
         customer.Customer.CityId = request.CityId;
         customer.Customer.DistrictId = request.DistrictId;
 
-        await dbContext.SaveChangesAsync(cancellationToken);
+        await unitOfWork.SaveChangesAsync(cancellationToken);
 
         return Response<CustomerDto>.Success(customer.Customer.FromEntity());
     }
