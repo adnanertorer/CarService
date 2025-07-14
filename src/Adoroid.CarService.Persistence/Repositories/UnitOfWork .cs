87745a88fbfef256acd38bac9a24d611088b4e94
adnanertorer <adnanertorer@hotmail.com>
2025-07-14 14:52:24 +0300

    companyrepository

--- src/Adoroid.CarService.Persistence/Repositories/UnitOfWork .cs
+++ src/Adoroid.CarService.Persistence/Repositories/UnitOfWork .cs
@@ -1,4 +1,5 @@
 ﻿using Adoroid.CarService.Application.Common.Abstractions;
+using Adoroid.CarService.Application.Features.Companies.Abstracts;
 using Adoroid.CarService.Application.Features.Users.Abtracts.Repositories;
 
 namespace Adoroid.CarService.Persistence.Repositories;
@@ -7,11 +8,13 @@ public class UnitOfWork : IUnitOfWork
 {
     private readonly CarServiceDbContext _dbContext;
     public IUserRepository Users { get; }
+    public ICompanyReposiyory Companies { get; }
 
     public UnitOfWork(CarServiceDbContext dbContext)
     {
         _dbContext = dbContext ?? throw new ArgumentNullException(nameof(dbContext));
         Users = new UserRepository(_dbContext);
+        Companies = new CompanyRepository(_dbContext);
     }
 
     public async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
