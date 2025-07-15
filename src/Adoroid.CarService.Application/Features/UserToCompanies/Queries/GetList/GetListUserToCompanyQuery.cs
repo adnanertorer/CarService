93efaa9364a46cb1dbf5088e284654840ec03579
adnanertorer <adnanertorer@hotmail.com>
2025-07-15 17:05:59 +0300

    IUserToCompanyRepository

--- src/Adoroid.CarService.Application/Features/UserToCompanies/Queries/GetList/GetListUserToCompanyQuery.cs
+++ src/Adoroid.CarService.Application/Features/UserToCompanies/Queries/GetList/GetListUserToCompanyQuery.cs
@@ -1,12 +1,11 @@
-﻿using Adoroid.CarService.Application.Common.Abstractions.Auth;
+﻿using Adoroid.CarService.Application.Common.Abstractions;
+using Adoroid.CarService.Application.Common.Abstractions.Auth;
 using Adoroid.CarService.Application.Common.Extensions;
 using Adoroid.CarService.Application.Features.UserToCompanies.Dtos;
 using Adoroid.CarService.Application.Features.UserToCompanies.MappingExtensions;
-using Adoroid.CarService.Persistence;
 using Adoroid.Core.Application.Requests;
 using Adoroid.Core.Application.Wrappers;
 using Adoroid.Core.Repository.Paging;
-using Microsoft.EntityFrameworkCore;
 using MinimalMediatR.Core;
 
 namespace Adoroid.CarService.Application.Features.UserToCompanies.Queries.GetList;
@@ -14,18 +13,14 @@ namespace Adoroid.CarService.Application.Features.UserToCompanies.Queries.GetLis
 public record GetListUserToCompanyQuery(PageRequest PageRequest)
     : IRequest<Response<Paginate<UserToCompanyDto>>>;
 
-public class GetListUserToCompanyQueryHandler(CarServiceDbContext dbContext, ICurrentUser currentUser)
+public class GetListUserToCompanyQueryHandler(IUnitOfWork unitOfWork, ICurrentUser currentUser)
     : IRequestHandler<GetListUserToCompanyQuery, Response<Paginate<UserToCompanyDto>>>
 {
     public async Task<Response<Paginate<UserToCompanyDto>>> Handle(GetListUserToCompanyQuery request, CancellationToken cancellationToken)
     {
         var companyId = currentUser.ValidCompanyId();
 
-        var query = dbContext.UserToCompanies
-            .Include(i => i.Company)
-            .Include(i => i.User)
-            .AsNoTracking()
-            .Where(i => i.CompanyId == companyId);
+        var query = unitOfWork.UserToCompanies.GetQueryable(companyId);
 
             var result = await query
                 .OrderByDescending(i => i.CreatedDate)
---
+++ src/Adoroid.CarService.Persistence/Repositories/UserToCompanyRepository.cs
@@ -0,0 +1,50 @@
+﻿using Adoroid.CarService.Application.Features.UserToCompanies.Abstracts;
+using Adoroid.CarService.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Adoroid.CarService.Persistence.Repositories;
+
+public class UserToCompanyRepository(CarServiceDbContext dbContext) : IUserToCompanyRepository
+{
+    public async Task AddAsync(UserToCompany userToCompany, CancellationToken cancellationToken)
+    {
+        await dbContext.UserToCompanies.AddAsync(userToCompany, cancellationToken);
+    }
+
+    public async Task<UserToCompany?> GetByIdAsync(Guid id, bool asNoTracking, CancellationToken cancellationToken)
+    {
+        var qeury = asNoTracking
+            ? dbContext.UserToCompanies.AsNoTracking()
+            : dbContext.UserToCompanies;
+
+        return await qeury.FirstOrDefaultAsync(i => i.Id == id, cancellationToken);
+    }
+
+    public async Task<UserToCompany?> GetByIdWithIncludedAsync(Guid id, bool asNoTracking, CancellationToken cancellationToken)
+    {
+        var qeury = asNoTracking
+           ? dbContext.UserToCompanies.Include(e => e.Company)
+            .Include(e => e.User)
+            .AsNoTracking()
+           : dbContext.UserToCompanies.Include(e => e.Company)
+            .Include(e => e.User);
+
+        return await qeury.FirstOrDefaultAsync(i => i.Id == id, cancellationToken);
+    }
+
+    public IQueryable<UserToCompany> GetQueryable(Guid companyId)
+    {
+        return dbContext.UserToCompanies
+            .Include(i => i.Company)
+            .Include(i => i.User)
+            .AsNoTracking()
+            .Where(i => i.CompanyId == companyId);
+    }
+
+    public async Task<bool> IsExists(Guid userId, Guid companyId, CancellationToken cancellationToken)
+    {
+        return await dbContext.UserToCompanies
+            .AsNoTracking()
+            .AnyAsync(i => i.UserId == userId && i.CompanyId == companyId, cancellationToken);
+    }
+}
