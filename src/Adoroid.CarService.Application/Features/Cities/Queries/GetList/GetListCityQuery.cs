17af33cba8459c07215bd6087b28e587d4363d90
adnanertorer <adnanertorer@hotmail.com>
2025-07-15 15:18:17 +0300

    added city repository

--- src/Adoroid.CarService.Application/Features/Cities/Queries/GetList/GetListCityQuery.cs
+++ src/Adoroid.CarService.Application/Features/Cities/Queries/GetList/GetListCityQuery.cs
@@ -1,17 +1,16 @@
-﻿using Adoroid.CarService.Application.Common.Abstractions.Caching;
+﻿using Adoroid.CarService.Application.Common.Abstractions;
+using Adoroid.CarService.Application.Common.Abstractions.Caching;
 using Adoroid.CarService.Application.Common.Extensions;
 using Adoroid.CarService.Domain.Entities;
-using Adoroid.CarService.Persistence;
 using Adoroid.Core.Application.Wrappers;
 using Adoroid.Core.Repository.Paging;
-using Microsoft.EntityFrameworkCore;
 using MinimalMediatR.Core;
 
 namespace Adoroid.CarService.Application.Features.Cities.Queries.GetList;
 
 public record GetListCityQuery() : IRequest<Response<Paginate<City>>>;
 
-public class GetListCityQueryHandler(CarServiceDbContext dbContext, ICacheService cacheService) : IRequestHandler<GetListCityQuery, Response<Paginate<City>>>
+public class GetListCityQueryHandler(IUnitOfWork unitOfWork, ICacheService cacheService) : IRequestHandler<GetListCityQuery, Response<Paginate<City>>>
 {
     const string redisKeyPrefix = "city:list";
     public async Task<Response<Paginate<City>>> Handle(GetListCityQuery request, CancellationToken cancellationToken)
@@ -19,12 +18,9 @@ public class GetListCityQueryHandler(CarServiceDbContext dbContext, ICacheServic
 
         var list = await cacheService.GetOrSetPaginateAsync<List<City>>(redisKeyPrefix, async () =>
         {
-            var query = dbContext.Cities
-                .AsNoTracking();
+            var list = await unitOfWork.Cities.GetAllAsync(cancellationToken);
+            return [.. list];
 
-            return await query
-                  .OrderBy(i => i.Name)
-                  .ToListAsync(cancellationToken);
         }, TimeSpan.FromHours(7));
 
         return Response<Paginate<City>>.Success(list.AsQueryable().ToPaginate(0, 100));
