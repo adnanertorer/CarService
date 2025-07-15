c79d3e14e06306b291f28700c50bc83c21a03efe
adnanertorer <adnanertorer@hotmail.com>
2025-07-15 15:26:51 +0300

    added districts repository

---
+++ src/Adoroid.CarService.Persistence/Repositories/DistrictRepository.cs
@@ -0,0 +1,17 @@
+﻿using Adoroid.CarService.Application.Features.Districts.Abstracts;
+using Adoroid.CarService.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Adoroid.CarService.Persistence.Repositories;
+
+public class DistrictRepository(CarServiceDbContext dbContext) : IDistrictRepository
+{
+    public async Task<IEnumerable<District>> GetDistricts(int cityId, CancellationToken cancellationToken = default)
+    {
+        return await dbContext.Districts
+            .AsNoTracking()
+            .Where(d => d.CityId == cityId)
+            .OrderBy(d => d.Name)
+            .ToListAsync(cancellationToken);
+    }
+}
