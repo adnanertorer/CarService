550a483ef5d483382d19565a82abea39c7c90e19
adnanertorer <adnanertorer@hotmail.com>
2025-07-24 16:04:26 +0300

    suppliers

--- src/Adoroid.CarService.Application/Features/Suppliers/Commands/Update/Validations/UpdateSupplierCommandValidator.cs
+++ src/Adoroid.CarService.Application/Features/Suppliers/Commands/Update/Validations/UpdateSupplierCommandValidator.cs
@@ -12,33 +12,31 @@ public class UpdateSupplierCommandValidator : AbstractValidator<UpdateSupplierCo
             .WithMessage(string.Format(ValidationMessages.Required, "Id"));
 
         RuleFor(x => x.Email)
-           .NotEmpty()
-           .WithMessage(string.Format(ValidationMessages.Required, "E-Posta"))
            .EmailAddress()
-           .WithMessage(string.Format(ValidationMessages.Email, "E-Posta"))
+           .WithMessage(string.Format(ValidationMessages.Email, "E-Posta")).When(x => !string.IsNullOrWhiteSpace(x.Email))
            .MaximumLength(60)
-           .WithMessage(string.Format(ValidationMessages.MaxLength, "60"));
+           .WithMessage(string.Format(ValidationMessages.MaxLength, "E-Posta", "60")).When(x => !string.IsNullOrWhiteSpace(x.Email));
 
         RuleFor(x => x.Address)
             .MaximumLength(250)
-            .WithMessage(string.Format(ValidationMessages.MaxLength, "250"));
+            .WithMessage(string.Format(ValidationMessages.MaxLength, "Adres", "250"));
 
         RuleFor(x => x.ContactName)
             .NotEmpty()
             .WithMessage(string.Format(ValidationMessages.Required, "Yetkili adı"))
             .MaximumLength(50)
-            .WithMessage(string.Format(ValidationMessages.MaxLength, "50"));
+            .WithMessage(string.Format(ValidationMessages.MaxLength, "Yetkili adı", "50"));
 
         RuleFor(x => x.Name)
             .NotEmpty()
             .WithMessage(string.Format(ValidationMessages.Required, "Tedarikçi adı"))
             .MaximumLength(150)
-            .WithMessage(string.Format(ValidationMessages.MaxLength, "16"));
+            .WithMessage(string.Format(ValidationMessages.MaxLength, "Tedarikçi adı", "16"));
 
         RuleFor(x => x.PhoneNumber)
             .NotEmpty()
             .WithMessage(string.Format(ValidationMessages.Required, "Telefon"))
             .MaximumLength(20)
-            .WithMessage(string.Format(ValidationMessages.MaxLength, "20"));
+            .WithMessage(string.Format(ValidationMessages.MaxLength, "Telefon", "20"));
     }
 }
---
+++ src/Adoroid.CarService.Persistence/Migrations/20250724111724_alter_column_max_lenght_supplier.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Adoroid.CarService.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class alter_column_max_lenght_supplier : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Suppliers",
+                type: "character varying(250)",
+                maxLength: 250,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(50)",
+                oldMaxLength: 50);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Suppliers",
+                type: "character varying(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(250)",
+                oldMaxLength: 250);
+        }
+    }
+}
