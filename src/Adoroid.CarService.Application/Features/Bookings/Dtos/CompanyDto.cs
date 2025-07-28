64c45938458d6df3a33ea40175422417c9461cda
adnanertorer <adnanertorer@hotmail.com>
2025-07-28 15:46:25 +0300

    added CreateBookingCommand

---
+++ src/Adoroid.CarService.Application/Features/Bookings/Dtos/CompanyDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Adoroid.CarService.Application.Features.Bookings.Dtos;
+
+public class CompanyDto
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public string AuthorizedPerson { get; set; }
+    public string PhoneNumber { get; set; }
+    public string Address { get; set; }
+}
---
+++ src/Adoroid.CarService.Application/Features/Bookings/ExceptionMessages/BusinessExceptionMessages.cs
@@ -0,0 +1,8 @@
+﻿namespace Adoroid.CarService.Application.Features.Bookings.ExceptionMessages;
+
+public class BusinessExceptionMessages
+{
+    public const string NotFound = "Randevu bulunamadı.";
+    public const string AlreadyExists = "Bu randevu talebi zaten var.";
+    public const string CompanyNotFound = "Firma bulunamadı.";
+}
