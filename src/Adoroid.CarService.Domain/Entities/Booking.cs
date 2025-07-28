e32efecbd584b74b3ed7c46e69a1edd13593abcf
adnanertorer <adnanertorer@hotmail.com>
2025-07-28 13:09:18 +0300

    added entity, configuration and migration

---
+++ src/Adoroid.CarService.Domain/Entities/Booking.cs
@@ -0,0 +1,19 @@
+﻿using Adoroid.Core.Repository.Repositories;
+
+namespace Adoroid.CarService.Domain.Entities;
+
+public class Booking : Entity<Guid>
+{
+    public Guid MobileUserId { get; set; }
+    public Guid CompanyId { get; set; }
+    public DateTime BookingDate { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public string VehicleBrand { get; set; }
+    public string VehicleModel { get; set; }
+    public int VehicleYear { get; set; }
+    public bool IsApproved { get; set; }
+
+    public MobileUser MobileUser { get; set; }
+    public Company Company { get; set; }
+}
