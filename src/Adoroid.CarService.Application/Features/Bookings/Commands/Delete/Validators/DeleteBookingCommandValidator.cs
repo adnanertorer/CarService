024b9c9e86666c78e2c54b02869262308d2c208a
adnanertorer <adnanertorer@hotmail.com>
2025-07-28 16:00:59 +0300

    added DeleteBookingCommand

---
+++ src/Adoroid.CarService.Application/Features/Bookings/Commands/Delete/Validators/DeleteBookingCommandValidator.cs
@@ -0,0 +1,16 @@
+﻿using Adoroid.CarService.Application.Common.ValidationMessages;
+using FluentValidation;
+
+namespace Adoroid.CarService.Application.Features.Bookings.Commands.Delete.Validators;
+
+public class DeleteBookingCommandValidator : AbstractValidator<DeleteBookingCommand>
+{
+    public DeleteBookingCommandValidator()
+    {
+        RuleFor(x => x.BookingId)
+            .NotEmpty()
+            .WithMessage(string.Format(ValidationMessages.Required, "Randevu Id"))
+            .NotNull()
+            .WithMessage(string.Format(ValidationMessages.NotNull, "Randevu Id"));
+    }
+}
