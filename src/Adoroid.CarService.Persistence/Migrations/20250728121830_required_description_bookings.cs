f373df19d3a5647545fe2d9ecfcaf758de49cb46
adnanertorer <adnanertorer@hotmail.com>
2025-07-28 15:46:25 +0300

    added CreateBookingCommand

---
+++ src/Adoroid.CarService.Persistence/Migrations/20250728121830_required_description_bookings.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Adoroid.CarService.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class required_description_bookings : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Bookings_Companies_CompanyId",
+                table: "Bookings");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Bookings_MobileUsers_MobileUserId",
+                table: "Bookings");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Bookings_Companies_CompanyId",
+                table: "Bookings",
+                column: "CompanyId",
+                principalTable: "Companies",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Bookings_MobileUsers_MobileUserId",
+                table: "Bookings",
+                column: "MobileUserId",
+                principalTable: "MobileUsers",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Bookings_Companies_CompanyId",
+                table: "Bookings");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Bookings_MobileUsers_MobileUserId",
+                table: "Bookings");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Bookings_Companies_CompanyId",
+                table: "Bookings",
+                column: "CompanyId",
+                principalTable: "Companies",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Bookings_MobileUsers_MobileUserId",
+                table: "Bookings",
+                column: "MobileUserId",
+                principalTable: "MobileUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
