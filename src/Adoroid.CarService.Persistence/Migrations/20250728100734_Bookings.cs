fe44d7024405174e240a35b2e8178f4b510c4e55
adnanertorer <adnanertorer@hotmail.com>
2025-07-28 13:09:18 +0300

    added entity, configuration and migration

---
+++ src/Adoroid.CarService.Persistence/Migrations/20250728100734_Bookings.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Adoroid.CarService.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class Bookings : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Bookings",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    MobileUserId = table.Column<Guid>(type: "uuid", nullable: false),
+                    CompanyId = table.Column<Guid>(type: "uuid", nullable: false),
+                    BookingDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Title = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    Description = table.Column<string>(type: "character varying(500)", maxLength: 500, nullable: false),
+                    VehicleBrand = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    VehicleModel = table.Column<string>(type: "character varying(30)", maxLength: 30, nullable: false),
+                    VehicleYear = table.Column<int>(type: "integer", nullable: false),
+                    IsApproved = table.Column<bool>(type: "boolean", nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    UpdatedDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    DeletedDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    CreatedBy = table.Column<Guid>(type: "uuid", maxLength: 64, nullable: false),
+                    UpdatedBy = table.Column<Guid>(type: "uuid", maxLength: 64, nullable: false),
+                    DeletedBy = table.Column<Guid>(type: "uuid", maxLength: 64, nullable: false),
+                    IsDeleted = table.Column<bool>(type: "boolean", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Bookings", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Bookings_Companies_CompanyId",
+                        column: x => x.CompanyId,
+                        principalTable: "Companies",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.NoAction);
+                    table.ForeignKey(
+                        name: "FK_Bookings_MobileUsers_MobileUserId",
+                        column: x => x.MobileUserId,
+                        principalTable: "MobileUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.NoAction);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Bookings_CompanyId",
+                table: "Bookings",
+                column: "CompanyId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Bookings_MobileUserId",
+                table: "Bookings",
+                column: "MobileUserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Bookings");
+        }
+    }
+}
