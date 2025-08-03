c65caf3fe2d9ff6238177fcd5bcbe86abc89ec6f
adnanertorer <adnanertorer@hotmail.com>
2025-08-03 17:42:18 +0300

    added GetCustomerDebtQuery handler

---
+++ src/Adoroid.CarService.Application/Features/AccountTransactions/Dtos/CustomerDebtDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Adoroid.CarService.Application.Features.AccountTransactions.Dtos;
+
+public class CustomerDebtDto
+{
+    public Guid CustomerId { get; set; } 
+    public string CustomerName { get; set; } 
+    public string CustomerSurname { get; set; } 
+    public decimal Balance { get; set; } 
+    public List<AccountTransactionDto> Transactions { get; set; } = new List<AccountTransactionDto>();
+}
