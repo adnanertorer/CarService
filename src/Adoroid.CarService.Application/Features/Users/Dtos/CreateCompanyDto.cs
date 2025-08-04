dbf9dee241becf3bc87ecb304194654f0d9caac3
adnanertorer <adnanertorer@hotmail.com>
2025-08-04 14:08:47 +0300

    added CreateCompanyUserCommand

---
+++ src/Adoroid.CarService.Application/Features/Users/Dtos/CreateCompanyDto.cs
@@ -0,0 +1,15 @@
+﻿namespace Adoroid.CarService.Application.Features.Users.Dtos;
+
+public class CreateCompanyDto
+{
+    public string CompanyName { get; set; }
+    public string AuthorizedName { get; set; }
+    public string AuthorizedSurname { get; set; }
+    public string TaxNumber { get; set; }
+    public string TaxOffice { get; set; }
+    public int CityId { get; set; }
+    public int DistrictId { get; set; }
+    public string CompanyAddress { get; set; }
+    public string CompanyPhone { get; set; }
+    public string CompanyEmail { get; set; }
+}
