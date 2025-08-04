a2f390e7b8f7728a7995ad7a5cf6c44f124638c6
adnanertorer <adnanertorer@hotmail.com>
2025-08-04 14:36:28 +0300

    added endpoint

---
+++ src/Adoroid.CarService.Application/Features/Users/Commands/Create/CreateCompanyUserRequest.cs
@@ -0,0 +1,5 @@
+﻿using Adoroid.CarService.Application.Features.Users.Dtos;
+
+namespace Adoroid.CarService.Application.Features.Users.Commands.Create;
+
+public record CreateCompanyUserRequest(CreateUserDto User, CreateCompanyDto Company);
