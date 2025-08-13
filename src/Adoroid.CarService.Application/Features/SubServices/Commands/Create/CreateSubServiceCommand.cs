860ccbc36a8484224ce117af99a96e2863fc975a
adnanertorer <adnanertorer@hotmail.com>
2025-08-13 14:56:10 +0300

    rename record parameter

--- src/Adoroid.CarService.Application/Features/SubServices/Commands/Create/CreateSubServiceCommand.cs
+++ src/Adoroid.CarService.Application/Features/SubServices/Commands/Create/CreateSubServiceCommand.cs
@@ -13,7 +13,7 @@ using MinimalMediatR.Core;
 namespace Adoroid.CarService.Application.Features.SubServices.Commands.Create;
 
 public record CreateSubServiceCommand(Guid MainServiceId, string Operation, Guid EmployeeId, DateTime OperationDate, string? Description,
-    string? Material, string? MaterialBrand, decimal? MaterailCost, Guid? SupplierId, decimal? Discount, decimal Cost) : IRequest<Response<SubServiceDto>>;
+    string? Material, string? MaterialBrand, decimal? MaterialCost, Guid? SupplierId, decimal? Discount, decimal Cost) : IRequest<Response<SubServiceDto>>;
 
 public class CreateSubServiceCommandHandler(IUnitOfWork unitOfWork, ICurrentUser currentUser, ICacheService cacheService, ILogger<CreateSubServiceCommandHandler> logger)
     : IRequestHandler<CreateSubServiceCommand, Response<SubServiceDto>>
@@ -50,7 +50,7 @@ public class CreateSubServiceCommandHandler(IUnitOfWork unitOfWork, ICurrentUser
             OperationDate = request.OperationDate,
             CreatedBy = Guid.Parse(currentUser.Id!),
             CreatedDate = DateTime.UtcNow,
-            MaterialCost = request.MaterailCost
+            MaterialCost = request.MaterialCost
         };
 
         var result = await unitOfWork.SubServices.AddAsync(entity, cancellationToken);
