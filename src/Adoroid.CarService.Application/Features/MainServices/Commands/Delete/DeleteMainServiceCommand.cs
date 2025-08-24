d3ba56d3a7be4581535f88c6b13cb4f189299447
adnanertorer <adnanertorer@hotmail.com>
2025-08-24 13:30:15 +0300

    removed caching main_service

--- src/Adoroid.CarService.Application/Features/MainServices/Commands/Delete/DeleteMainServiceCommand.cs
+++ src/Adoroid.CarService.Application/Features/MainServices/Commands/Delete/DeleteMainServiceCommand.cs
@@ -1,6 +1,5 @@
 ﻿using Adoroid.CarService.Application.Common.Abstractions;
 using Adoroid.CarService.Application.Common.Abstractions.Auth;
-using Adoroid.CarService.Application.Common.Abstractions.Caching;
 using Adoroid.CarService.Application.Common.Extensions;
 using Adoroid.CarService.Application.Features.SubServices.ExceptionMessages;
 using Adoroid.Core.Application.Wrappers;
@@ -11,11 +10,9 @@ namespace Adoroid.CarService.Application.Features.MainServices.Commands.Delete
 {
     public record DeleteMainServiceCommand(Guid Id) : IRequest<Response<Guid>>;
 
-    public class DeleteMainServiceCommandHandler(IUnitOfWork unitOfWork, ICurrentUser currentUser, ICacheService cacheService, ILogger<DeleteMainServiceCommandHandler> logger)
+    public class DeleteMainServiceCommandHandler(IUnitOfWork unitOfWork, ICurrentUser currentUser, ILogger<DeleteMainServiceCommandHandler> logger)
         : IRequestHandler<DeleteMainServiceCommand, Response<Guid>>
     {
-        const string redisKeyPrefix = "mainservice:list";
-        const string redisSubServiceKeyPrefix = "subservice:list";
         public async Task<Response<Guid>> Handle(DeleteMainServiceCommand request, CancellationToken cancellationToken)
         {
             var companyId = currentUser.ValidCompanyId();
@@ -35,23 +32,11 @@ namespace Adoroid.CarService.Application.Features.MainServices.Commands.Delete
                 item.IsDeleted = true;
                 item.DeletedBy = Guid.Parse(currentUser.Id!);
                 item.DeletedDate = DateTime.UtcNow;
-
-                var cacheSubServiceKey = $"{redisSubServiceKeyPrefix}:{companyId}";
-                await cacheService.RemoveFromListAsync<dynamic>(cacheSubServiceKey, item.Id.ToString());
             }
 
             await unitOfWork.SaveChangesAsync(cancellationToken);
 
-            try
-            {
-                var cacheListKey = $"{redisKeyPrefix}:{companyId}";
-                await cacheService.RemoveFromListAsync<dynamic>(cacheListKey, request.Id.ToString());
-            }
-            catch(Exception ex)
-            {
-                logger.LogError(ex, "Error while deleting to cache for main service delete.");
-            }
-           
+            logger.LogInformation("Main service with id {Id} deleted by user {UserId}", entity.Id, currentUser.Id);
 
             return Response<Guid>.Success(entity.Id);
         }
